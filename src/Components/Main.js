396d05c00e998b141656942824aa9b63132770c3
Juan C Olmedo <doubleparked88@gmail.com>
2023-04-13 04:22:57 -0700

    kinda get it it works do

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,68 +2,57 @@ import React from "react";
 import HornedBeast from "./HornedBeast";
 import SelectedBeast from "./SelectedBeast";
 
-
 class Main extends React.Component {
+  //NOTE: constructor with props passed to initiate state
   constructor(props) {
     super(props);
     this.state = {
       showModal: false,
-      selectedBeast:null
+      //NOTE: selected beast for object reference
+      selectedBeast: null,
     };
   }
 
-  setshowModal(value){
+  handleClose = () => {
+    this.setState({ showModal: false });
+  };
+
+  //NOTE: brings in the selected beast object and sets it to the state and sets showmodal to true
+  handleOpen = (selectedBeast) => {
     this.setState({
-        showModal:value,
-       
+      selectedBeast: selectedBeast,
+      showModal: true,
     });
-}
-handleClose = () => {
-  console.log('made it here');
-  this.setState({showModal: false})
-}
+  };
 
-handleOpen = () => {
-  console.log("atleast made it here");
-  //alert('this kind of works');
-  this.setState({showModal: true})
-}
-  
   render() {
-
-    //data used for beast rendering
     const dataForBeasts = this.props.jsonData;
     return (
-    <main>
-    {
-     dataForBeasts.map((createdAnimals) =>  
-    <HornedBeast
-      setshowModal={this.props.setshowModal}
-      className="myAnimal"
-      key={createdAnimals._id}
-      imageSRC={createdAnimals.image_url}
-      altName={createdAnimals.keyword}
-      title={createdAnimals.title}
-      description={createdAnimals.description}
-      horns={createdAnimals.horns}
-      data = {createdAnimals.title}
-      handleOpen={this.handleOpen}
-      showTheModal={this.state.showModal}
-      closeTheModal={this.handleClose}
-      />
-     )
-     }
-    
-    {
-      this.state.SelectedBeast &&
-      <SelectedBeast
-      show={this.state.showModal}
-      onClose={this.handleClose}
-      />
+      <main>
+        {dataForBeasts.map((createdAnimals) => (
+          <HornedBeast
+            className="myAnimal"
+            key={createdAnimals._id}
+            imageSRC={createdAnimals.image_url}
+            altName={createdAnimals.keyword}
+            title={createdAnimals.title}
+            description={createdAnimals.description}
+            horns={createdAnimals.horns}
+            data={createdAnimals.title}
+            //NOTE:need funtion below to set hanfle open to the current Animal by using the arrow function here you can wait untill the onClick event occurs if no arrow function is there then the function will be called at time of creation causing last element to pop up and get stuck there
+            handleOpen={() => this.handleOpen(createdAnimals)}
+          />
+        ))}
 
-    }
-     </main> 
-    )
+        {this.state.selectedBeast && (
+          <SelectedBeast
+            show={this.state.showModal}
+            onClose={this.handleClose} //sets showModal to False
+            selectedBeast={this.state.selectedBeast}
+          />
+        )}
+      </main>
+    );
   }
 }
 
--- src/Components/SelectedBeast.js
+++ src/Components/SelectedBeast.js
@@ -1,51 +1,28 @@
 import React from "react";
-import { Modal } from "react-bootstrap";
-import { Button } from "bootstrap";
+import { Modal, Button } from "react-bootstrap";
 
 class SelectedBeast extends React.Component {
-  // constructor(props){
-  //   super(props);
-  //     this.state = {
-  //        moodalShow:false,
-  //        setModalShow:false
-  //     }
-  //   };
-  
   render() {
     return (
-
-      <Modal show={this.props.show} onHide={this.props.onClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Overdraft Warning</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>This is the last time!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={this.props.onClose}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
-
-      //   <Modal
-      //  show={this.props.showTheModal} 
-      //  onHide={this.props.closeTheModal} 
-      //  animation={false}
-      //   >
-      //     <Modal.Header closeButton>
-      //       <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
-      //     </Modal.Header>
-      //     <Modal.Body>
-      //       <h4>Centered Modal</h4>
-      //       <p>
-      //         Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-      //         dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
-      //         ac consectetur ac, vestibulum at eros.
-      //       </p>
-      //     </Modal.Body>
-      //     <Modal.Footer>
-      //       <Button onClick="this.props.hideTheModal">Close</Button>
-      //     </Modal.Footer>
-      //   </Modal>
+      <Modal show={this.props.show} onHide={this.props.onClose}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>{this.props.selectedBeast.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        {/* NOTE:need to select subelements of props because React does not know what to render */}
+          <img src={this.props.selectedBeast.image_url} alt={this.props.selectedBeast.title} style={{width: "100%"}} />
+          <p>{this.props.selectedBeast.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={this.props.onClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     );
   }
 }
